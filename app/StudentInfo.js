119c2b0d0bd88cac0891c10ff47f98f6d7560e28
Rapanda0 <ryel.rapada07@gmail.com>
2023-09-14 14:36:48 -0600

    slight fix for github link

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <main>
-            <p>Ryel Rapada</p>
-            <p>CPRG 306 F</p>
-            <Link href="https://github.com/Rapanda0/cprg306-assignments.git"></Link>
+            <p>Name: Ryel Rapada</p>
+            <p>Course section: CPRG 306 F</p>
+            <Link href="https://github.com/Rapanda0/cprg306-assignments">https://github.com/Rapanda0/cprg306-assignments</Link>
         </main>
     )
 }
