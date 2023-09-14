fd7f4f8a619339364d7863cf50efc81516ae4b21
Rapanda0 <ryel.rapada07@gmail.com>
2023-09-14 14:32:57 -0600

    week 2 stuff
    did the week 2 assignment

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Ryel Rapada</p>
+            <p>CPRG 306 F</p>
+            <Link href="https://github.com/Rapanda0/cprg306-assignments.git"></Link>
+        </main>
+    )
+}
