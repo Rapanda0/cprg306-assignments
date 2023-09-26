ac95afaf996f74adaabdf4aa3153c4d03a4edc36
Rapanda0 <ryel.rapada07@gmail.com>
2023-09-26 11:09:46 -0600

    removed pointless code

--- app/week3/page.js
+++ app/week3/page.js
@@ -1,10 +1,8 @@
-import Item from "./item";
 import ItemList from "./item-list";
 
 export default function Week3() {
     return (
     <main>
-        <h1 className='text-center text-xl font-bold '>My Shopping List</h1>
         <ItemList/>
     </main>
     );
