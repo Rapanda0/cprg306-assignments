462730aee7f4c320a8541eef8acf7d2f3dd92a37
Rapanda0 <ryel.rapada07@gmail.com>
2023-10-09 00:17:32 -0600

    week4 assignment added

--- app/page.js
+++ app/page.js
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import StudentInfo from './StudentInfo'
 import Link from 'next/link'
+import { Dai_Banna_SIL } from 'next/font/google'
 
 export default function Home() {
   return (
     <main>
       <h1 className='font-bold text-xl text-center'>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <Link href='/week2'>Week 2</Link>
-      <Link href='/week3'>Week 3</Link>
+      <div className='flex flex-col'>
+        <Link href='/week2'>Week 2</Link>
+        <Link href='/week3'>Week 3</Link>
+        <Link href='/week4'>Week 4</Link>
+      </div>
     </main>
   )
 }
